08bc70ea8595af6029c3324a2e843531b8b42e3f
Eduarda <eduarda.roberta.hf@gmail.com>
2019-06-03 21:31:37 -0300

    Novo mundo

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('filme', views.filme, name='filme'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
     ]
 
 
